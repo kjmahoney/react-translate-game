dfc30342ed17ed0aed7e414683ec2c63f3915eae
Kevin Mahoney <kjmahoney13@gmail.com>
2019-12-02 07:19:44 -0500

    render first sprite

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,10 +1,28 @@
-import React, {useEffect}  from "react";
-import { init } from "kontra";
-import styles from './Canvas.module.scss';
+import React, { useEffect } from "react";
+import { init, Sprite, GameLoop } from "kontra";
+import styles from "./Canvas.module.scss";
 
 const Canvas = () => {
   useEffect(() => {
-    init();
+    const { canvas } = init();
+
+    let road = Sprite({
+      x: 100, 
+      y: 80,
+      color: "red", 
+      width: 20, 
+      height: 40,
+    });
+
+    let loop = GameLoop({
+      update: function() {
+      },
+      render: function() {
+        road.render();
+      }
+    });
+
+    loop.start(); // start the game
   });
 
   return <canvas className={styles.canvas}></canvas>;
