a526feb97369c64b1a0244b158ccd8b4452d1d7d
Kevin Mahoney <kjmahoney13@gmail.com>
2019-12-23 23:32:36 -0500

    improve collision

--- src/utils/collision.js
+++ src/utils/collision.js
@@ -1,10 +1,14 @@
 export const collision = (ball, player, canvas) => {
   //still need to account for circumference
   if (
-    ball.x >= player.x &&
-    ball.x <= player.x + player.width &&
+    ball.x >= player.x - ball.radius &&
+    ball.x <= player.x + player.width + ball.radius &&
     ball.y === canvas.height - player.height - ball.radius
   ) {
     ball.dy = -ball.dy;
+  }
+  if (ball.y === canvas.height - ball.radius) {
+    //placeholder for now
+    ball.dy = 0;
   }
 };
